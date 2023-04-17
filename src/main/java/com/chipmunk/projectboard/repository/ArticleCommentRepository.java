1c5cd09834eb4e2f6c0470ca4e7b8dd4710018b7
chipmunk-dev <devquokkajeong@gmail.com>
2023-04-17 10:19:20 +0900

    #10 - Repository interface 작성
    이걸로 db에 접근

---
+++ src/main/java/com/chipmunk/projectboard/repository/ArticleCommentRepository.java
@@ -0,0 +1,7 @@
+package com.chipmunk.projectboard.repository;
+
+import com.chipmunk.projectboard.domain.ArticleComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+}
