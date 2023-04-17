087ca36f4d949ed7a738cb54eab6331ec346889f
chipmunk-dev <devquokkajeong@gmail.com>
2023-04-17 23:47:46 +0900

    # 3 - data rest 설정

--- src/main/java/com/chipmunk/projectboard/repository/ArticleCommentRepository.java
+++ src/main/java/com/chipmunk/projectboard/repository/ArticleCommentRepository.java
@@ -2,6 +2,8 @@ package com.chipmunk.projectboard.repository;
 
 import com.chipmunk.projectboard.domain.ArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 }
